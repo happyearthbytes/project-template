ae74a1e76dd92f96c9b6a84c7cffa21bc3a0ec0a
Joseph DiPilato <josephdipilato@Josephs-iMac.home>
2022-11-13 18:00:21 -0500

    two-sum

--- apps/leetcode/problems/two-sum/two-sum.mine.cpp
+++ apps/leetcode/problems/two-sum/two-sum.mine.cpp
@@ -1,16 +1,65 @@
-// Given an array of integers <code>nums</code>&nbsp;and an integer <code>target</code>, return <em>indices of the two numbers such that they add up to <code>target</code></em>.</p>
+// Given an array of integers <code>nums</code>&nbsp;
+// and an integer <code>target</code>, return <em>indices of the two numbers
+// such that they add up to <code>target</code></em>.</p>
+#include <algorithm>
+# include <iterator>
 # include <vector>
 # include <iostream>
+# include <cassert>
+# include <unordered_map>
+
 
 class Solution {
 public:
-    std::vector<int> twoSum(std::vector<int>& nums, int target) {
-        std::vector<int> thing;
-        return thing;
+    std::vector<int> result;
+    std::vector<int> expected;
+    std::vector<int> calculate(std::vector<int>& nums, int target) {
+        std::vector<int> rval;
+        std::unordered_map<int, int> hm;
+
+        for (int i=0; i<nums.size(); ++i) {
+            if (hm.count(target - nums[i])) {
+                rval.push_back(i);
+                rval.push_back(hm[target - nums[i]]);
+                break;
+            }
+            else{
+                hm[nums[i]] = i;
+            }
+        }
+        return rval;
+    }
+
+    void run(void) {
+      std::vector<int> nums{1,2,3,4,5,6,7,8,9,10,11,12};
+      int target = 17;
+      result = calculate(nums, target);
+    }
+    void set_expect(void) {
+      int src[] = {8,7};
+      expected.assign(std::begin(src), std::end(src));
+    }
+
+    void test(void) {
+        set_expect();
+        run();
+
+        std::cout << "Expected: ";
+        for (const auto& i: expected)
+            std::cout << i << ' ';
+        std::cout << std::endl;
+        std::cout << "Result: ";
+        for (const auto& i: result)
+            std::cout << i << ' ';
+        std::cout << std::endl;
     }
 };
 
+
 int main(int nargs, char** args) {
-    std::cout << "Hello" << std::endl;
+    std::cout << "-----" << std::endl;
+    Solution code = Solution();
+    code.test();
+    std::cout << "-----" << std::endl;
     return 0;
 }
