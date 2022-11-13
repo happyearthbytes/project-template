457a59fbffe8ab5eed7a3de02f3113846b15fb0b
Joseph DiPilato <josephdipilato@Josephs-iMac.home>
2022-11-13 14:31:26 -0500

    add initial leetcode

---
+++ apps/leetcode/scrape.sh
@@ -0,0 +1,24 @@
+URL=https://leetcode.com/discuss/general-discussion/460599/blind-75-leetcode-questions
+
+# "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" --headless --dump-dom ${URL} > out.html
+cat out.html  | sed 's/>/>\n/g' | grep leetcode | grep problems | sed 's|.*\(https.*\)/">|\1|' > links.txt
+
+while read p; do
+  NAME=$(echo "$p" | sed 's|.*/||')
+  echo "$NAME"
+  DIRNAME=problems/"$NAME"
+  mkdir -p "${DIRNAME}"
+  touch "${DIRNAME}"/"${NAME}".orig.cpp
+  touch "${DIRNAME}"/"${NAME}".cpp
+  touch "${DIRNAME}"/"${NAME}".html
+  touch "${DIRNAME}"/"${NAME}".instructions.txt
+  touch "${DIRNAME}"/"${NAME}".code.txt
+  echo "$p" > "${DIRNAME}"/"${NAME}".url.txt
+  # "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" --headless --dump-dom "${p}" > "${DIRNAME}"/"${NAME}".html
+  cat "${DIRNAME}"/"${NAME}".html | sed -n '\, question-content, s| question-content_.*"><div><p>|\n\n---\n\n|p' | tail -1 > "${DIRNAME}"/"${NAME}".instructions.txt
+  cat "${DIRNAME}"/"${NAME}".instructions.txt | sed 's/^/# /' > "${DIRNAME}"/"${NAME}".orig.cpp
+  cat "${DIRNAME}"/"${NAME}".html | sed 's/>/>\n/g' | grep " CodeMirror-line " -A20 | sed -n '\,^[^<], p' | egrep -v "^--$" | sed -E 's|<(/)?span.*||' | sed 's|.*</div>|<newline>\n|' | sed 's|&nbsp;| |g' | tr -d '\n' | sed 's/<newline>/\n/g' > "${DIRNAME}"/"${NAME}".code.txt
+  cat "${DIRNAME}"/"${NAME}".code.txt >> "${DIRNAME}"/"${NAME}".orig.cpp
+  echo "" > "${DIRNAME}"/"${NAME}".orig.cpp
+
+done < links.txt
